81df7d5039c54059573604851b73a9dd171129fd
Shahid <109452231+raza-shahi@users.noreply.github.com>
2026-01-11 22:36:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function makeid(l) {
   // write your code here
+	let res = "";
+	const charList = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	for (let i = 0; i < l; i++) {
+		res+= charList.charAt((Math.random()*charList.length));
+	}
+	return res;
 }
 
 // Do not change the code below.
