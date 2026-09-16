c52f2fe8099825c878f3fc1b6368482794997e84
Shahid <109452231+raza-shahi@users.noreply.github.com>
2026-09-16 20:08:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function makeid(l) {
 	 let alphanumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
   let res = "";
   for(let i=1;i<=l;i++){
-    let random = Math.floor(Math.random()*62)
+    let random = Math.floor(Math.random()*alphanumeric.length)
     res += alphanumeric[random]
   }
   return res;
